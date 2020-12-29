fb26b6bb5dffb4da9ad50689bad864d063f88603
kurocraft0312 <kurocraft0312@gmail.com>
2020-12-29 23:54:55 +0900

    add:service/module/controllerファイルをそれぞれ生成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ApiModule],
+  controllers: [AppController, ApiController],
   providers: [AppService],
 })
 export class AppModule {}
