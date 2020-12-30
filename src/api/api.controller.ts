dd2921efd1d51f14ad83fe8b80a71466a11c843e
kurocraft0312 <kurocraft0312@gmail.com>
2020-12-30 14:12:06 +0900

    WIP:難しいのでPHPに一旦回帰

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -3,11 +3,13 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('api')
 export class ApiController {
 
-    @Get()
-    async TwAPI() {
-        const url = "";
+    // @Get()
+    // TODO: 難しいのでPHPに一旦回帰
+    // async TwAPI() {
+    //     const url = "https://api.twitter.com/2/users/:id/tweets";
 
-    }
+
+    // }
 
     // @Post()
     // create()
